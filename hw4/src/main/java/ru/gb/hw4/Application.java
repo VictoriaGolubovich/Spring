e927c405ec5a2785b703dee8f69f48f49f988d1a
vika <vmliechko@mail.ru>
2024-09-21 14:18:32 +0300

    change hw4

--- hw4/src/main/java/ru/gb/hw4/Application.java
+++ hw4/src/main/java/ru/gb/hw4/Application.java
@@ -1,4 +1,8 @@
 package ru.gb.hw4;
+/**
+ * Разработайте веб-приложение с использованием Spring MVC и Thymeleaf, которое
+ * отображает список пользователей из базы данных.
+ */
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
